c81997ca0ceb1724cc6ff11ea12ec76863b8e1b4
Kush Bhogate <kushbg1610@gmail.com>
2025-10-16 09:26:17 +0530

    final changes

--- pages/1_predict_dnf.py
+++ pages/1_predict_dnf.py
@@ -14,13 +14,13 @@ st.title("🚦 F1 Race Outcome Prediction")
 st.markdown("Enter race details below to predict whether the driver will **Finish** or **DNF**.")
 
 with st.form("dnf_form"):
-    grid = st.number_input("Grid Position", min_value=1, max_value=24, step=1)
-    year = st.number_input("Year", min_value=1950, max_value=2024, step=1)
+    grid = st.number_input("Grid Position", min_value=1, max_value=34, step=1)
+    year = st.number_input("Year", min_value=1950, max_value=2025, step=1)
     age = st.number_input("Driver Age", min_value=18, max_value=59, step=1)
 
     driver = st.selectbox("Driver", data['drivers'])
     constructor = st.selectbox("Constructor", data['constructors'])
-    circuit_id = st.number_input("Circuit ID", min_value=1, max_value=100, step=1)
+    circuit_id = st.number_input("Circuit ID", min_value=1, max_value=80, step=1)
 
     submitted = st.form_submit_button("Predict 🚀")
 
